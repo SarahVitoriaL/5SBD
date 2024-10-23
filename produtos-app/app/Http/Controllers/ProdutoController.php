8c3649985023f238ee5c2935aed36310539227e9
SarahVitoriaL <142323165+SarahVitoriaL@users.noreply.github.com>
2024-10-22 23:18:28 -0300

    Add files via upload

---
+++ produtos-app/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|max:255',
+            'valor' => 'required|numeric',
+        ]);
+
+        Produto::create($request->all());
+        return redirect()->route('produtos.index');
+    }
+
+    public function edit(Produto $produto)
+    {
+        return view('produtos.edit', compact('produto'));
+    }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required|max:255',
+            'valor' => 'required|numeric',
+        ]);
+
+        $produto->update($request->all());
+        return redirect()->route('produtos.index');
+    }
+
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+        return redirect()->route('produtos.index');
+    }
+}
+
---
+++ produtos-app/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\ProdutoController;
+
+Route::resource('produtos', ProdutoController::class);
+
